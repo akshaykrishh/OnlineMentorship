ac30d76c7b9d584e4f32bfa60f070d77ae6c12c4
Akshay Krishna <akshaykrishnakanrh@gmail.com>
2020-06-04 04:07:58 +0530

    added firebase

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $('#ans').on('change', function () {
     if ($(this).val() === "yes") {
-        $("#ifYes").show()
+        $("#ifYes").show();
     }
     else {
         $("#ifYes").hide()
@@ -31,4 +31,70 @@ window.onclick = function (event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
+}
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyCiTQa7jnHJY9FD5Y0z2WrmsKw3v9Lw2K0",
+    authDomain: "tensorsonlinementorship.firebaseapp.com",
+    databaseURL: "https://tensorsonlinementorship.firebaseio.com",
+    projectId: "tensorsonlinementorship",
+    storageBucket: "tensorsonlinementorship.appspot.com",
+    messagingSenderId: "261109200684",
+    appId: "1:261109200684:web:f175bec6b79553381cde89",
+    measurementId: "G-9N6WGL97PX"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+// Reference messages collection
+var messagesRef = firebase.database().ref('messages');
+
+//Listen for form submit
+document.getElementById('registerForm').addEventListener('submit', submitForm);
+
+
+//submit form
+function submitForm(e) {
+    e.preventDefault();
+
+
+    //Get values
+    var Fname = getInputVal('Fname');
+    var Lname = getInputVal('Lname');
+    var email = getInputVal('email');
+    var phone = getInputVal('phone');
+    var ans = getInputVal('ans');
+    var Cname = getInputVal('Cname');
+    var address = getInputVal('address');
+    var checkbox = getInputVal('checkbox');
+
+    //Save Message
+    saveMessage(Fname, Lname, email, phone, ans, Cname, address, checkbox)
+
+
+}
+
+//Function to get form values
+
+function getInputVal(id) {
+    return document.getElementById(id).value;
+}
+
+//Save message to firebase
+function saveMessage(Fname, Lname, email, phone, ans, Cname, address) {
+    var newMessageRef = messagesRef.push();
+    newMessageRef.set({
+        Fname: Fname,
+        Lname: Lname,
+        email: email,
+        phone: phone,
+        ans: ans,
+        Cname: Cname,
+        address: address,
+        checkbox: checkbox
+
+    });
 }
